ca3ef4c8163fed9fdda646261f160c9c406c4b88
124001 <sebastianc018@gmail.com>
2017-07-16 17:02:25 -0300

    ver_21

--- miviajante_v21/vista/templates_c/d9408ab2e72dac2d3f370ce8a23dabc66ab66197_0.file.login.tpl.php
+++ miviajante_v21/vista/templates_c/d9408ab2e72dac2d3f370ce8a23dabc66ab66197_0.file.login.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-07-16 15:08:10
+/* Smarty version 3.1.30, created on 2017-07-16 17:00:20
   from "C:\xampp\htdocs\PAW\miviajante_v20\vista\templates\login.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_596bab8a682895_15731427',
+  'unifunc' => 'content_596bc5d4584367_27973318',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'd9408ab2e72dac2d3f370ce8a23dabc66ab66197' => 
     array (
       0 => 'C:\\xampp\\htdocs\\PAW\\miviajante_v20\\vista\\templates\\login.tpl',
-      1 => 1500228363,
+      1 => 1500235178,
       2 => 'file',
     ),
   ),
@@ -21,18 +21,18 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:p_layout.tpl' => 1,
   ),
 ),false)) {
-function content_596bab8a682895_15731427 (Smarty_Internal_Template $_smarty_tpl) {
+function content_596bc5d4584367_27973318 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_17121596bab8a67e486_70487211', 'body');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_21559596bc5d45818c9_77991640', 'body');
 $_smarty_tpl->inheritance->endChild();
 $_smarty_tpl->_subTemplateRender("file:p_layout.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
 }
 /* {block 'body'} */
-class Block_17121596bab8a67e486_70487211 extends Smarty_Internal_Block
+class Block_21559596bc5d45818c9_77991640 extends Smarty_Internal_Block
 {
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
@@ -62,7 +62,7 @@ index.php/Login/iniciarSesion" method="post">
                         <input class="boton" type="submit" value="Iniciar Sesion">
                         <p id="aclaracion">(*) Campo requerido.<p>
                         <h5><a href="<?php echo $_smarty_tpl->tpl_vars['path_base']->value;?>
-index.php/LoginGoogle/iniciarSesion" class="enlaceChico">Iniciar con cuenta de Google</a></h5>
+index.php/LoginGoogle/iniciarSesion" class="enlaceGoogle">ENTRAR CON GOOGLE</a></h5>
                         <h5>Sos nuevo? -> <a href="<?php echo $_smarty_tpl->tpl_vars['path_base']->value;?>
 index.php/Registro/mostrar" class="enlaceChico">REGISTRATE</a></h5>
                     </li>
